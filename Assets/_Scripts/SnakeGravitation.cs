b1fcd70d46cb5fd200ba6b5007d52e6dab7629b9
Alexander Evstratov <alexdrugev@me.com>
2023-10-19 20:57:40 +0300

    Add Apple and snake on scene, make apple gravity.

---
+++ Assets/_Scripts/SnakeGravitation.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class SnakeGravitation : MonoBehaviour
+{
+    [SerializeField] private Transform _apple;
+    private Rigidbody _rigidbody;
+    public float gravityForce;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        Vector3 gravity = (transform.position - _apple.position);
+        transform.rotation = Quaternion.FromToRotation(transform.up, gravity) * transform.rotation;
+        _rigidbody.AddForce(gravity * gravityForce * Time.deltaTime);
+    }
+}
